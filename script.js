55c18b10d938407e2c3fb1d8f484371e79809361
JoyOffiong <offiongjoyasuquo@gmail.com>
2022-06-18 04:22:03 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuButton=document.querySelector('.nav-toggle');
+const navigationMenu=document.querySelector('.mobile-links');
+const closeButton=document.querySelector('.nav-toggle2');
+const mainWrap = document.querySelector('.main-wrap')
+
+
+menuButton.addEventListener('click', () => {
+  
+        menuButton.style.display='none'
+        closeButton.style.display = 'block'
+        navigationMenu.style.transform = 'translateX(0%)'
+})
+ 
+    closeButton.addEventListener('click', () => {
+            closeButton.style.display='none'   
+            menuButton.style.display = 'block' 
+            navigationMenu.style.transform = 'translateX(100%)'
+
+            
+    })
