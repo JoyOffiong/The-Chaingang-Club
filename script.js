e0763a0124011881895014e8faccaac042c4ecff
JoyOffiong <offiongjoyasuquo@gmail.com>
2022-07-10 18:17:09 +0100

    minor edit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuButton=document.querySelector('.nav-toggle');
 const navigationMenu=document.querySelector('.mobile-links');
 const closeButton=document.querySelector('.nav-toggle2');
+const mainWrap=document.querySelector('.main')
 
 
 
@@ -8,12 +9,15 @@ menuButton.addEventListener('click', () => {
   
         menuButton.style.display='none'
         closeButton.style.display = 'block'
+        mainWrap.style.overflow= 'hidden'
         navigationMenu.style.transform = 'translateX(0%)'
+       
 })
  
     closeButton.addEventListener('click', () => {
             closeButton.style.display='none'   
             menuButton.style.display = 'block' 
+            mainWrap.style.overflow= 'scroll'
             navigationMenu.style.transform = 'translateX(100%)'
 
             
